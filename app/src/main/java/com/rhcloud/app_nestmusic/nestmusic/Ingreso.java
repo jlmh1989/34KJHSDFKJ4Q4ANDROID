355334f9ec778086a303047f9e58ac9e7f597e85
jlmh1989 <jlmh1989@gmail.com>
2014-12-16 12:43:24 -0600

    first commit

---
+++ app/src/main/java/com/rhcloud/app_nestmusic/nestmusic/Ingreso.java
@@ -0,0 +1,29 @@
+package com.rhcloud.app_nestmusic.nestmusic;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+
+
+public class Ingreso extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingreso);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/rhcloud/app_nestmusic/nestmusic/MainActivity.java
@@ -0,0 +1,26 @@
+package com.rhcloud.app_nestmusic.nestmusic;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void registroOnClick(View view){
+        Intent intent = new Intent(this, Registro.class);
+        startActivity(intent);
+    }
+
+    public void ingresoOnClick(View view){
+        Intent intent = new Intent(this, Ingreso.class);
+        startActivity(intent);
+    }
+}
